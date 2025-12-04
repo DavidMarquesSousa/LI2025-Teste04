bd0c9f20ea6274f6e3e5bb5573a43769a0c8274b
NormalPerson <153537220+DavidMarquesSousa@users.noreply.github.com>
2025-12-04 15:11:26 +0000

    5ª tarefa cumprida

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 //ficheiro de script
+(() => {
+
+  //Selecionar formulário
+  const forms = document.querySelectorAll('.needs-validation')
+
+  //loop
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
